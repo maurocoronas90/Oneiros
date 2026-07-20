32e62378cef75fabe4df997fee07cd51aef37c64
maurocoronas90 <maurocoronas90@gmail.com>
2026-07-20 04:46:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,9 @@
-const frases = [
-"Ogni sogno appartiene a qualcuno.",
-"Finché qualcuno non lo ruba.",
-"Benvenuto in ONEIROS."
-];
+const pdf = "ONEIROS_PILOT_Mobile_Edition_COMPRESSO.pdf";
 
-let i = 0;
+document.getElementById("logo").onclick = () => {
+    window.open(pdf, "_blank");
+};
 
-setInterval(() => {
-    i++;
-    if (i >= frases.length) i = 0;
-    document.getElementById("phrase").innerText = frases[i];
-}, 3500);
-
-document.getElementById("enter").onclick = () => {
-    window.location.href = "ONEIROS_PILOT_Mobile_Edition(2).pdf";
+document.getElementById("readButton").onclick = () => {
+    window.open(pdf, "_blank");
 };
